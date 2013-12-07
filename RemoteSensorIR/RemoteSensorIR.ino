d5998e28effa58f5a74a87433c5a0ee299bd27d6
tonussi <lptonussi@gmail.com>
2013-12-07 15:02:02 -0200

    Printa leitura de decimal => hexadecimal

--- RemoteSensorIR/RemoteSensorIR.ino
+++ RemoteSensorIR/RemoteSensorIR.ino
@@ -1,32 +1,18 @@
 #include <IRremote.h>
-IRsend irsend;
 
-int RECV_PIN = 11;
+int RECV_PIN = 6;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(57600);
   irrecv.enableIRIn(); // Start the receiver
 }
 
 void loop() {
   if (irrecv.decode(&results)) {
-    Serial.println(results.value, DEC);
+    Serial.println(results.value, HEX);
     irrecv.resume(); // Receive the next value
   }
 }
-
-void sendRemote() {
-  if (Serial.read() != -1) {
-    for (int i = 0; i < 3; i++) {
-      irsend.sendSony(0xa90, 12); // Sony TV power code
-      delay(100);
-    }
-  }
-}
-
-
-
-
